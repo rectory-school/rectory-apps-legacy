99f5c2588dd49767b7f91a1ae54af560431b2aec
Adam M. Peacock <adam.peacock@rectoryschool.org>
2015-12-03 15:46:52 -0500

    Better admin for academics

--- academics/admin.py
+++ academics/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from academics.models import Dorm
+from academics.models import Dorm, AcademicYear
 
 class DormAdmin(admin.ModelAdmin):
     filter_horizontal = ['heads']
 
+class AcademicYearAdmin(admin.ModelAdmin):
+    fields = ['year', 'current']
+    list_display = ['year', 'current']
+    readonly_fields = ['year']
+
 # Register your models here.
 admin.site.register(Dorm, DormAdmin)
+admin.site.register(AcademicYear, AcademicYearAdmin)
