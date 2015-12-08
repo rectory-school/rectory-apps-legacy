7100d07e8ab3ccc05a57ae2428a3f9caadd9ef99
Adam M. Peacock <adam.peacock@rectoryschool.org>
2015-12-08 13:11:23 -0500

    Styling and functionality on the survey frontend

--- courseevaluations/static/js/survey.js
+++ courseevaluations/static/js/survey.js
@@ -1,19 +1,34 @@
+var recordClicked = false;
+
 $(document).ready(function() {
-	$("a.helpLink").on("click", function(e, ui) {
-		
-		id = $(this).attr("href")
-		
-		$("#" + id).dialog({
-		      modal: true,
-			  buttons: {
-			      Ok: function() {
-			          $( this ).dialog( "close" );
-			      }
-			  }
-		    });
-			
-		e.preventDefault();
-		
-		
+  $("a.helpLink").on("click", function(e, ui) {
+
+    id = $(this).attr("href")
+
+    $("#" + id).dialog({
+      modal: true,
+      buttons: {
+        Ok: function() {
+          $(this).dialog("close");
+        }
+      }
+    });
+
+    e.preventDefault();
+
+
+  });
+  
+  $(".btnSave").click(function (e) {
+    recordClicked = true;
+  });
+  
+	$(window).bind('beforeunload', function(event) {
+		if (! recordClicked) {
+			event.stopPropagation();
+			event.returnValue = "Warning: You are about to leave this page, but you have not saved your changes. Please scroll to the bottom and click Record my Answers"
+			return event.returnValue;
+		}
 	});
+  
 });
