b27e900623cb37fbefa5ad2febf8f62ff3d490ce
Adam M. Peacock <adam.peacock@rectoryschool.org>
2015-12-09 10:46:01 -0500

    Broke evaluation views out into multiple files

--- courseevaluations/urls.py
+++ courseevaluations/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^student/landing/$', 'courseevaluations.views.student_landing', name='courseevaluations_student_landing'),
-    url(r'^student/survey/$', 'courseevaluations.views.student_survey', name='courseevaluations_student_survey'),
+    url(r'^student/landing/$', 'courseevaluations.views.student.student_landing', name='courseevaluations_student_landing'),
+    url(r'^student/survey/$', 'courseevaluations.views.student.student_survey', name='courseevaluations_student_survey'),
 ]
