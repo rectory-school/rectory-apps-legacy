a01a559a82d0354e6e6285d1f909ea959c2c6188
Adam Peacock <adam.peacock@rectoryschool.org>
2015-12-09 22:40:04 -0600

    Slow down e-mail sending to not hit SES rate limit

--- courseevaluations/lib/async.py
+++ courseevaluations/lib/async.py
@@ -1,8 +1,10 @@
 from courseevaluations.models import StudentEmailTemplate
 from academics.models import Student
 from django.core.mail import send_mail
+import time
 
 def send_student_email_from_template(template_id, student_id, override_email=None):
+    time.sleep(1) #Make an e-mail take at least one second to slow down for SES
     student = Student.objects.get(pk=student_id)
     template = StudentEmailTemplate.objects.get(pk=template_id)
     
@@ -14,6 +16,7 @@ def send_student_email_from_template(template_id, student_id, override_email=Non
     msg.send()
 
 def send_confirmation_email(addresses, to_addresses):
+    time.sleep(1) #Make an e-mail take at least one second to slow down for SES
     body = "Your e-mail was sent to the following {count:} people: \n\n{addresses:}".format(count=len(addresses), addresses="\n".join(addresses))
     
     send_mail("Message confirmation", body, "technology@rectoryschool.org", to_addresses)
