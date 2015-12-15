18b05c0681055d209de5edadf100c0df185837a6
Adam M. Peacock <adam.peacock@rectoryschool.org>
2015-12-15 09:55:23 -0500

    Initial work on polymorphic admins

--- courseevaluations/admin.py
+++ courseevaluations/admin.py
@@ -6,7 +6,7 @@ from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModel
 
 from adminsortable.admin import SortableAdmin, NonSortableParentAdmin, SortableStackedInline
 
-from courseevaluations.models import QuestionSet, FreeformQuestion, MultipleChoiceQuestion, MultipleChoiceQuestionOption, EvaluationSet, DormParentEvaluation, CourseEvaluation, IIPEvaluation, MultipleChoiceQuestionAnswer, FreeformQuestionAnswer, StudentEmailTemplate, Evaluable
+from courseevaluations.models import QuestionSet, FreeformQuestion, MultipleChoiceQuestion, MultipleChoiceQuestionOption, EvaluationSet, DormParentEvaluation, CourseEvaluation, IIPEvaluation, MultipleChoiceQuestionAnswer, FreeformQuestionAnswer, StudentEmailTemplate, Evaluable, DormEvaluation
 from academics.models import Student
 
 class MultipleChoiceQuestionOptionInline(SortableStackedInline):
@@ -69,6 +69,40 @@ class MultipleChoiceQuestionAdmin(SortableAdmin):
 class QuestionSetAdmin(NonSortableParentAdmin):
     inlines = [MultipleChoiceQuestionInline, FreeformQuestionInline]
 
+class EvaluableChildAdmin(PolymorphicChildModelAdmin):
+    base_model = Evaluable
+    
+    base_fieldsets = (
+        ('Basic Information', {'fields': ('student', 'evaluation_set', 'question_set', 'complete')}),
+    )
+
+class CourseEvaluationAdmin(EvaluableChildAdmin):
+    base_model = CourseEvaluation
+
+class IIPEvaluationAdmin(EvaluableChildAdmin):
+    base_model = IIPEvaluation
+
+class DormEvaluationAdmin(EvaluableChildAdmin):
+    base_model = DormEvaluation
+    
+class DormParentEvaluationAdmin(DormEvaluationAdmin):
+    base_model = DormParentEvaluation
+    
+class EvaluableParentAdmin(PolymorphicParentModelAdmin):
+    base_model = Evaluable
+    
+    polymorphic_list = True
+    
+    list_filter = ['student']
+    list_display = ['student_display', 'student', 'evaluation_type_title', '__str__']
+    
+    
+    child_models = (
+        (CourseEvaluation, CourseEvaluationAdmin),
+        (IIPEvaluation, IIPEvaluationAdmin),
+        (DormEvaluation, DormEvaluationAdmin),
+        (DormParentEvaluation, DormParentEvaluationAdmin),
+    )
     
 # Register your models here.
 admin.site.register(QuestionSet, QuestionSetAdmin)
@@ -76,3 +110,4 @@ admin.site.register(FreeformQuestion, SortableAdmin)
 admin.site.register(MultipleChoiceQuestion, MultipleChoiceQuestionAdmin)
 admin.site.register(EvaluationSet)
 admin.site.register(StudentEmailTemplate)
+admin.site.register(Evaluable, EvaluableParentAdmin)
