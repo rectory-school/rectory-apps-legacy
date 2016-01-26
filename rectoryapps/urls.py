4942874e23731cd6eb01815a61737607a147df53
Adam M. Peacock <adam.peacock@rectoryschool.org>
2016-01-26 14:36:08 -0500

    Serve media in development

--- rectoryapps/urls.py
+++ rectoryapps/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^evaluations/', include('courseevaluations.urls')),
     url(r'^django-rq/', include('django_rq.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls'))
 ]
+
+if settings.DEBUG:
+  urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
