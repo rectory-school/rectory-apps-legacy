4b5d6f228d72871f3dfb6d768ffeba667d7c69c3
Adam M. Peacock <adam.peacock@rectoryschool.org>
2016-01-29 17:06:15 -0500

    Basic view using the calendar maker

--- calendar_generator/urls.py
+++ calendar_generator/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^text/(?P<id>[0-9]+)/$', 'calendar_generator.views.calendar_days_text', name='calendar_generator_days_text'),
+    url(r'^pdf/(?P<id>[0-9]+)/$', 'calendar_generator.views.full_calendar_pdf', name='calendar_generator_days_pdf'),
 ]
--- calendar_generator/views.py
+++ calendar_generator/views.py
@@ -1,8 +1,15 @@
+from datetime import date
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+
 from calendar_generator.models import Calendar
-from calendar_generator.lib import get_days
+from calendar_generator.lib import get_days, structured_calendar_layout
+from calendar_generator.pdf_generator.calendar_maker import GridDrawer, GridFormatter
 
 # Create your views here.
 def calendar_days_text(request, id):
@@ -15,5 +22,60 @@ def calendar_days_text(request, id):
         line = "{date:}: {day:}\n".format(date=date.strftime("%Y-%m-%d"), day=days[date].letter)
         
         response.write(line)
+    
+    print(structured_calendar_layout(days, False))
+    
+    return response
+
+def full_calendar_pdf(request, id):
+    calendar = get_object_or_404(Calendar, pk=id)
+    days = get_days(calendar)
+    
+    header_days = []
+    if calendar.monday:
+        header_days.append(0)
+
+    if calendar.tuesday:
+        header_days.append(1)
+    
+    if calendar.wednesday:
+        header_days.append(2)
+    
+    if calendar.thursday:
+        header_days.append(3)
+    
+    if calendar.friday:
+        header_days.append(4)
+    
+    if calendar.saturday:
+        header_days.append(5)
+    
+    if calendar.sunday:
+        header_days.append(6)
+    
+    
+    structured_data = structured_calendar_layout(days, False)
+    
+    response = HttpResponse(content_type="application/pdf")
+    
+    pdf = canvas.Canvas(response, pagesize=(11*inch, 8.5*inch))
+    
+    for year, month in sorted(structured_data.keys()):
+        grid = structured_data[(year, month)]
+        
+        first_of_month = date(year, month, 1)
+        month_title = first_of_month.strftime("%B")
+        
+        grid_formatter = GridFormatter(header_font="HelveticaNeue-Bold", day_font = "HelveticaNeue-Light", date_font="HelveticaNeue-Light")
+        grid_formatter.header_background = colors.black
+        grid_formatter.header_line = colors.white
+        grid_formatter.header_color = colors.white
+        
+        grid_drawer = GridDrawer(grid, header_days, grid_formatter)
+        
+        grid_drawer.draw_on(pdf, .5*inch, 8*inch, 10*inch, 7.5*inch, 1)
+        pdf.showPage()
+        
+    pdf.save()
     
     return response
