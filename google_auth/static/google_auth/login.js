4166f2425f4795779b8c748d2056c89bc7ce0287
Adam M. Peacock <adam.peacock@rectoryschool.org>
2016-02-11 15:07:53 -0500

    Working Google logins

--- google_auth/static/google_auth/login.js
+++ google_auth/static/google_auth/login.js
@@ -12,13 +12,24 @@ $("document").ready(function() {
     $("#django-signon-form").dialog("open");
   })
   
-  
-  
   $("#django-signon-form").dialog({
     autoOpen: autoOpenDialog,
     width: 500,
     movable: false,
   })
+  
+  var csrftoken = $.cookie('csrftoken');
+  
+  $.ajaxSetup({
+      beforeSend: function(xhr, settings) {
+          if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+              // Send the token to same-origin, relative URLs only.
+              // Send the token only if the method warrants CSRF protection
+              // Using the CSRFToken value acquired earlier
+              xhr.setRequestHeader("X-CSRFToken", csrftoken);
+          }
+      }
+  });
   
   startGoogleApp();
 });
@@ -44,16 +55,25 @@ function onAuth2Load() {
 }
 
 function attachClickHandler(auth2, elementID) {
-  auth2.attachClickHandler(elementID, {}, onSuccess, onError);
+  //auth2.attachClickHandler(elementID, {}, onSuccess, onError);
+  
+  $("#" + elementID).click(function(e) {
+    auth2.grantOfflineAccess({'redirect_uri': 'postmessage'}).then(onGoogleLoginSuccess);
+  })
 }
 
-function onSuccess(googleUser) {
-  var profile = googleUser.getBasicProfile();
+function onGoogleLoginSuccess(authResult) {
+  var processURL = Urls["google-auth:login"]()
   
-  jQueryAlert("Successfully signed in", profile.getName() + "<br />" + profile.getEmail());
-  console.log(googleUser);
-  
-  alert(Urls["google-auth:login"]())
+  $.post({
+    url: processURL,
+    data: {'code': authResult.code, logon_type: 'google'},
+    success: onDjangoLoginSuccess
+  });
+}
+
+function onDjangoLoginSuccess(data) {
+  window.location = $("meta[name=next]").attr("content");
 }
 
 function onError(error) {
@@ -81,6 +101,23 @@ function jQueryAlert(title, message) {
         $(this).dialog("close");
       }
     }
-  });
-  
+  }); 
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+function sameOrigin(url) {
+    // test that a given url is a same-origin URL
+    // url could be relative or scheme relative or absolute
+    var host = document.location.host; // host + port
+    var protocol = document.location.protocol;
+    var sr_origin = '//' + host;
+    var origin = protocol + sr_origin;
+    // Allow absolute or scheme relative URLs to same origin
+    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+        // or any other URL that isn't scheme relative or absolute i.e relative.
+        !(/^(\/\/|http:|https:).*/.test(url));
 }
--- google_auth/views.py
+++ google_auth/views.py
@@ -5,8 +5,16 @@ from django.utils.http import is_safe_url
 
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
+
+from django.http import JsonResponse
 
 from django.views.generic import View
+
+from apiclient import discovery
+import httplib2
+from oauth2client import client
+
 
 class LogonView(View):
     def get(self, request):
@@ -22,12 +30,47 @@ class LogonView(View):
     
     def handle_google_logon(self, request):
         print (request.POST)
-    
+        
+        auth_code = request.POST.get("code")
+        
+        if not auth_code:
+            raise ValueError()
+            
+        #TODO: Move to settings
+        CLIENT_SECRET_FILE = '/Users/adam.peacock/development/rectoryapps/client_secret_file.json'
+        
+        credentials = client.credentials_from_clientsecrets_and_code(CLIENT_SECRET_FILE,
+        ['https://www.googleapis.com/auth/drive.appdata', 'profile', 'email'],
+        auth_code)
+        
+        email = credentials.id_token['email']
+        hosted_domain = credentials.id_token.get('hd', "")
+        
+        desired_hosted_domain = settings.GOOGLE_HOSTED_DOMAIN
+        
+        if desired_hosted_domain and hosted_domain.lower() != desired_hosted_domain.lower():
+            raise ValueError()
+        
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user = User(email=email)
+            user.save()
+        
+        #We'll see if this works...
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        
+        auth_login(request, user)
+            
+        return JsonResponse({'status_code': 200, 'email': email})
+            
+            
     def login_page(self, request, processed_form=None, start_with_django_dialog=False):
         context = {
             'google_client_id': settings.GOOGLE_OAUTH_CLIENT_ID,
             'google_hosted_domain': settings.GOOGLE_HOSTED_DOMAIN,
             'start_django_form': start_with_django_dialog,
+            'next': request.GET.get('next', request.path)
         }
         
         if processed_form:
