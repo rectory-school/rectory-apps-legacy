12c08845424ef2ad06da0d947ef3ce1feae75416
Adam Peacock <adam@thepeacock.net>
2019-08-18 19:01:55 -0400

    URL updates

--- paw/urls.py
+++ paw/urls.py
@@ -8,8 +8,8 @@ from paw import views
 app_name = 'paw'
 
 urlpatterns = [
-    path('html/(?P<slug>[\w-]+)/$', views.static, name='paw_static'),
-    path('json/page/(?P<slug>[\w-]+)/$', views.json_from_page, name='paw_json_page'),
+    path('html/<slug:slug>/$', views.static, name='paw_static'),
+    path('json/page/<slug:slug>/$', views.json_from_page, name='paw_json_page'),
     path('json/email/', views.json_from_email, name='paw_json_email'),
     path('json/default/$', views.json_default, name='paw_json_default'),
 ]
--- seating_charts/urls.py
+++ seating_charts/urls.py
@@ -7,11 +7,11 @@ app_name = 'seating_charts'
 
 urlpatterns = [
     path('', seating_charts.views.display.index, name='seating_index'),
-    path('edit/(?P<id>[0-9]+)/', seating_charts.views.edit.seatingChartEditor, name='seating_chart_editor'),
-    path('edit/(?P<id>[0-9]+)/shuffle/', seating_charts.views.edit.shuffle, name='seating_chart_shuffle'),
-    path('edit/(?P<id>[0-9]+)/data/', seating_charts.views.edit.seatingChartData, name='seating_chart_data'),
-    path('edit/(?P<id>[0-9]+)/move/', seating_charts.views.edit.moveStudent, name='seating_chart_move'),
-    path('view/(?P<id>[0-9]+)/table/', seating_charts.views.display.seatingChartByTable, name='seating_chart_view_table'),
-    path('view/(?P<id>[0-9]+)/student/', seating_charts.views.display.seatingChartByStudent, name='seating_chart_view_student'),
-    path('view/(?P<id>[0-9]+)/inserts.pdf', seating_charts.views.display.seatingChartInsert, name='seating_chart_view_inserts'),   
+    path('edit/<int:id>/', seating_charts.views.edit.seatingChartEditor, name='seating_chart_editor'),
+    path('edit/<int:id>/shuffle/', seating_charts.views.edit.shuffle, name='seating_chart_shuffle'),
+    path('edit/<int:id>/data/', seating_charts.views.edit.seatingChartData, name='seating_chart_data'),
+    path('edit/<int:id>/move/', seating_charts.views.edit.moveStudent, name='seating_chart_move'),
+    path('view/<int:id>/table/', seating_charts.views.display.seatingChartByTable, name='seating_chart_view_table'),
+    path('view/<int:id>/student/', seating_charts.views.display.seatingChartByStudent, name='seating_chart_view_student'),
+    path('view/<int:id>/inserts.pdf', seating_charts.views.display.seatingChartInsert, name='seating_chart_view_inserts'),   
 ]
